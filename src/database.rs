1a2aa109b0c82e1236de2e8db84211ea795cbe18
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2023-03-05 13:20:00 +0100

    Update database.rs

--- src/database.rs
+++ src/database.rs
@@ -1,19 +1,12 @@
 use mysql::{Pool, PooledConn};
 
-pub struct Database{
+pub fn get_db_connection() -> Option<PooledConn> {
+    let url = "mysql://root:password@localhost:3306/sys";
+    let pool = Pool::new(url).unwrap();
 
-}
-impl Database
-{
-    pub fn get_db_connection() -> Option<PooledConn> {
-        let url = "mysql://root:password@localhost:3306/sys";
-        let pool = Pool::new(url).unwrap();
-    
-        let conn = pool.get_conn().ok();
-    
-        //let selected_payments:Vec<i32> = conn.query("select 1").unwrap();
-        //print!("{}",&selected_payments[0].to_string());
-        return conn;
-    }
-}
+    let conn = pool.get_conn().ok();
 
+    //let selected_payments:Vec<i32> = conn.query("select 1").unwrap();
+    //print!("{}",&selected_payments[0].to_string());
+    return conn;
+}
